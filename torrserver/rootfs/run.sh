5717aab78309057155b051e6a8145aba65bd771d
AATrubilin <aatrubilin@gmail.com>
2022-07-09 22:56:58 +0700

    Save TorrServer and config in shared host folder

--- torrserver/rootfs/run.sh
+++ torrserver/rootfs/run.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/with-contenv bashio
 
-TS_RUNFILE="/TorrServer"
+declare -A ARCH_MAP="( [armv7]=arm7 [amd64]=amd64 [i386]=386 )"
+
+TS_SOURCE="TorrServer-linux-${ARCH_MAP[${BUILD_ARCH}]}"
+TS_URL="https://github.com/YouROK/TorrServer/releases/latest/download/${TS_SOURCE}"
 TS_VERSION="$(curl -sL https://api.github.com/repos/YouROK/TorrServer/releases/latest | jq -r '.tag_name')"
+TS_RUNFILE="/share/torrserver/TorrServer-${TS_VERSION}"
 
-echo "Downloading TorrServer" $TS_VERSION
+if [[ ! -f "$TS_RUNFILE" ]]
+then
+		rm -f TorrServer-*
+		echo "Downloading ${TS_SOURCE}-${TS_VERSION}..."
+		wget -O $TS_RUNFILE $TS_URL -v
+		chmod a+x $TS_RUNFILE
+fi
 
-wget -O $TS_RUNFILE https://github.com/YouROK/TorrServer/releases/latest/download/TorrServer-linux-$BUILD_ARCH
-chmod a+x $TS_RUNFILE
-
+# On linux systems you need to set the environment variable before run
 export GODEBUG=madvdontneed=1
 
-echo "Starting TorrServer..."
+echo "Starting TorrServer-${TS_VERSION}..."
 $TS_RUNFILE
