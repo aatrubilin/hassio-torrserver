e09db628c73f5702172c35b77f694e41acabd2d1
AATrubilin <aatrubilin@gmail.com>
2023-08-15 17:24:32 +0700

    chore: add logs

--- torrserver/rootfs/run.sh
+++ torrserver/rootfs/run.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/with-contenv bashio
 
-echo "Prepare download"
 echo "Build arch: ${BUILD_ARCH}"
 
 declare -A ARCH_MAP="( [armv7]=arm7 [amd64]=amd64 [i386]=386 [aarch64]=arm64 )"
+
+echo "Set envs"
 
 TS_SOURCE="TorrServer-linux-${ARCH_MAP[${BUILD_ARCH}]}"
 TS_URL="https://github.com/YouROK/TorrServer/releases/latest/download/${TS_SOURCE}"
@@ -12,13 +13,16 @@ TS_RUNFILE="/share/torrserver/TorrServer-${TS_VERSION}"
 
 if [[ ! -f "$TS_RUNFILE" ]]
 then
+    echo "Removing old file"
 		rm -f TorrServer-*
 		echo "Downloading ${TS_SOURCE}-${TS_VERSION}..."
 		wget -O $TS_RUNFILE $TS_URL -v
+		echo "Set chmod"
 		chmod a+x $TS_RUNFILE
 fi
 
 # On linux systems you need to set the environment variable before run
+echo "Set GODEBUG env"
 export GODEBUG=madvdontneed=1
 
 echo "Starting TorrServer-${TS_VERSION}..."
