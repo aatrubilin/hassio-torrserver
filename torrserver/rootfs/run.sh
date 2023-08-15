b260bb107a1233118c611a3b01f98536251fb245
AATrubilin <aatrubilin@gmail.com>
2023-08-15 18:01:47 +0700

    chore: update logs

--- torrserver/rootfs/run.sh
+++ torrserver/rootfs/run.sh
@@ -1,33 +1,32 @@
 #!/usr/bin/with-contenv bashio
+set -euo pipefail
 
 echo "Build arch: ${BUILD_ARCH}"
 
 declare -A ARCH_MAP="( [armv7]=arm7 [amd64]=amd64 [i386]=386 [aarch64]=arm64 )"
 
-echo "Set envs"
-
 TS_SOURCE="TorrServer-linux-${ARCH_MAP[${BUILD_ARCH}]}"
+echo "TS_SOURCE=${TS_SOURCE}"
 TS_URL="https://github.com/YouROK/TorrServer/releases/latest/download/${TS_SOURCE}"
-
-echo "Checking latest torrserver version"
-
+echo "TS_URL=${TS_URL}"
 TS_VERSION="$(curl -sL https://api.github.com/repos/YouROK/TorrServer/releases/latest | jq -r '.tag_name')"
+echo "TS_VERSION=${TS_VERSION}"
 TS_RUNFILE="/share/torrserver/TorrServer-${TS_VERSION}"
+echo "TS_RUNFILE=${TS_RUNFILE}"
 
 echo "Checking existed torrserver"
 
 if [[ ! -f "$TS_RUNFILE" ]]
 then
-    echo "Removing old file"
+    echo "rm -f TorrServer-*"
 		rm -f TorrServer-*
 		echo "Downloading ${TS_SOURCE}-${TS_VERSION}..."
 		wget -O $TS_RUNFILE $TS_URL -v
-		echo "Set chmod"
+		echo "Change mode"
 		chmod a+x $TS_RUNFILE
 fi
 
 # On linux systems you need to set the environment variable before run
-echo "Set GODEBUG env"
 export GODEBUG=madvdontneed=1
 
 echo "Starting TorrServer-${TS_VERSION}..."
