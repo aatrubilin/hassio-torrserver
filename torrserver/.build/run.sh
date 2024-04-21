65def5c2c6069f7d0ac05fdcb331b56cf4162446
AATrubilin <aatrubilin@gmail.com>
2024-04-21 21:34:45 +0700

    chore: update dockerfile

--- torrserver/.build/run.sh
+++ torrserver/.build/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv bashio
 set -euo pipefail
 
-echo "Arch: ${BUILD_ARCH}"
-echo "Starting TorrServer-$(cat /VERSION)"
-ls -la /
-/torrserver
+echo "Starting TorrServer-$(cat /VERSION), arch: ${BUILD_ARCH}"
+
+RUN chmod a+x /torrserver
+/torrserver --path $TS_CONF_PATH --logpath $TS_LOG_PATH --torrentsdir $TS_TORR_DIR --port $TS_PORT
