dc19090d3e625d09403baccaeebb5b0ad0db4fad
AATrubilin <aatrubilin@gmail.com>
2024-04-22 03:50:04 +0700

    chore: fix build

--- torrserver/.build/run.sh
+++ torrserver/.build/run.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/with-contenv bashio
 set -euo pipefail
 
-export GODEBUG="madvdontneed=1"
+ARCH="$(bashio::config 'arch')"
+
+if [[ "$ARCH" = "auto" ]]
+then
+    ARCH=$BUILD_ARCH
+fi
 
 TS_VERSION=$(grep '\s*TS_VERSION: \w' /etc/build.yaml | sed 's/^.*: //')
+TS_EXEC="/addon_config/TorrServer-${ARCH}"
 
-echo "Starting TorrServer-${TS_VERSION}, arch: ${BUILD_ARCH}"
+echo "TorrServer version: ${TS_VERSION}"
+echo "TorrServer exec: ${TS_EXEC}"
+echo "Build arch: ${BUILD_ARCH}"
+echo "Selected arch: ${ARCH}"
 
-ls -la /
-
-cat /VERSION
-
-chmod a+x /torrserver
-
-FLAGS="--path $TS_CONF_PATH --logpath $TS_LOG_PATH --port $TS_PORT --torrentsdir $TS_TORR_DIR"
+if [[ ! -f "$TS_EXEC" ]]
+then
+		echo "Downloading ${TS_SOURCE}-${TS_VERSION}..."
+		wget -O $TS_EXEC https://github.com/aatrubilin/hassio-torrserver/releases/download/${TS_VERSION}/TorrServer-${ARCH} -nv
+		chmod a+x $TS_EXEC
+fi
 
 if [ ! -d $TS_CONF_PATH ]; then
   mkdir -p $TS_CONF_PATH
@@ -27,6 +35,10 @@ if [ ! -f $TS_LOG_PATH ]; then
   touch $TS_LOG_PATH
 fi
 
-echo "Running with: ${FLAGS}"
+export GODEBUG="madvdontneed=1"
 
-/torrserver $FLAGS
+FLAGS="--path $TS_CONF_PATH --logpath $TS_LOG_PATH --port $TS_PORT --torrentsdir $TS_TORR_DIR"
+
+echo "Starting ${TS_EXEC} ${FLAGS}"
+
+$TS_EXEC $FLAGS
