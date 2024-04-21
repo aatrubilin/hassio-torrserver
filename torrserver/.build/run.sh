ec01b6b07765e1f4bac461e754a8151aacb572a7
AATrubilin <aatrubilin@gmail.com>
2024-04-22 00:45:32 +0700

    chore: fix build

--- torrserver/.build/run.sh
+++ torrserver/.build/run.sh
@@ -8,9 +8,6 @@ echo "Starting TorrServer-${TS_VERSION}, arch: ${BUILD_ARCH}"
 chmod a+x /torrserver
 
 FLAGS="--path $TS_CONF_PATH --logpath $TS_LOG_PATH --port $TS_PORT --torrentsdir $TS_TORR_DIR"
-if [[ "$TS_HTTPAUTH" -eq 1 ]]; then FLAGS="${FLAGS} --httpauth"; fi
-if [[ "$TS_RDB" -eq 1 ]]; then FLAGS="${FLAGS} --rdb"; fi
-if [[ "$TS_DONTKILL" -eq 1 ]]; then FLAGS="${FLAGS} --dontkill"; fi
 
 if [ ! -d $TS_CONF_PATH ]; then
   mkdir -p $TS_CONF_PATH
