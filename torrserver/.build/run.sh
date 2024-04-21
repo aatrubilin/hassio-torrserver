afc5edee1f91f538238f53769b10b332587943c9
AATrubilin <aatrubilin@gmail.com>
2024-04-21 21:39:46 +0700

    chore: update dockerfile

--- torrserver/.build/run.sh
+++ torrserver/.build/run.sh
@@ -3,5 +3,7 @@ set -euo pipefail
 
 echo "Starting TorrServer-$(cat /VERSION), arch: ${BUILD_ARCH}"
 
+ls -la /
+
 chmod a+x /torrserver
 /torrserver --path $TS_CONF_PATH --logpath $TS_LOG_PATH --torrentsdir $TS_TORR_DIR --port $TS_PORT
