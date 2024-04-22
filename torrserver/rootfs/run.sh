f95e021945bd567619de66c40c30ea568af56b9e
AATrubilin <aatrubilin@gmail.com>
2024-04-22 08:07:24 +0700

    chore: dev

---
+++ torrserver/rootfs/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/with-contenv bashio
+set -euo pipefail
+
+if [ ! -d $TS_CONF_PATH ]; then
+  mkdir -p $TS_CONF_PATH
+fi
+
+if [ ! -d $TS_TORR_DIR ]; then
+  mkdir -p $TS_TORR_DIR
+fi
+
+if [ ! -f $TS_LOG_PATH ]; then
+  touch $TS_LOG_PATH
+fi
+
+export GODEBUG="madvdontneed=1"
+
+FLAGS="--path $TS_CONF_PATH --logpath $TS_LOG_PATH --port $TS_PORT --torrentsdir $TS_TORR_DIR"
+
+echo "Starting ${TS_EXEC} ${FLAGS}"
+
+$TS_EXEC $FLAGS
