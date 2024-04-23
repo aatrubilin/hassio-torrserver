daa8778a6474d2afe379abc8cb6fbb042aca532f
AATrubilin <aatrubilin@gmail.com>
2024-04-24 00:04:03 +0700

    chore: dev

--- torrserver/.build/rootfs/run.sh
+++ torrserver/.build/rootfs/run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/with-contenv bashio
 set -euo pipefail
 
+FLAGS="--path $TS_CONF_PATH --torrentsdir $TS_TORR_DIR --port $TS_PORT"
+
 # Create conf path if not exists
 if [ ! -d $TS_CONF_PATH ]; then
   mkdir -p $TS_CONF_PATH
@@ -15,6 +17,7 @@ fi
 if [[ "$(bashio::config 'httpauth')" = true ]]
 then
   bashio::log.info "HTTPAuth: enabled"
+  FLAGS="${FLAGS} --httpauth"
   ACCESS_DB="${TS_CONF_PATH}/accs.db"
   jq_args=(-n )
   for key in $(bashio::config "logins|keys"); do
@@ -23,10 +26,10 @@ then
 
     # Validate if username & password are not empty
     if bashio::config.is_empty "logins[${key}].username" || \
-      bashio::config.is_empty "logins[${key}].password"
+       bashio::config.is_empty "logins[${key}].password"
     then
       bashio::log.fatal
-      bashio::log.fatal 'Configuration of logins is incomplete.'
+      bashio::log.fatal 'Configuration of HTTP Auth is incomplete.'
       bashio::log.fatal
       bashio::log.fatal "username[${key}]: '${USERNAME}'"
       bashio::log.fatal "password[${key}]: '${PASSWORD}'"
@@ -52,7 +55,6 @@ fi
 
 # Starting torrserver
 export GODEBUG="madvdontneed=1"
-FLAGS="--path $TS_CONF_PATH --torrentsdir $TS_TORR_DIR --port $TS_PORT"
 bashio::log.info "Starting torrserver..."
 bashio::log.info "torrserver ${FLAGS}"
 torrserver $FLAGS
